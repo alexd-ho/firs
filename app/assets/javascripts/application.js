da091257a9a8fabf5478e9da6d51a8f5794be3d4
andymarshall-35 <167748704+andymarshall-35@users.noreply.github.com>
2024-05-08 13:49:54 +0100

    initial shot at 'add another' for activities

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,31 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
+
+    // var newNameId = 2;
+    // var addAnotherButton = document.getElementById('add-another-button');
+    // var names = document.getElementById('public-activities');
+
+    // var stringToHTML = function (str) {
+    //   var parser = new DOMParser();
+    //   var doc = parser.parseFromString(str, 'text/html');
+    //   return doc.body.firstChild;
+    // };
+
+    // addAnotherButton.onclick = function () {
+    //   var s = stringToHTML(`
+      
+    //   <div class='govuk-form-group'>
+    //     <label class="govuk-label" for='name-${newNameId}'>
+    //         Name
+    //       </label>
+    //     <input class="govuk-input" id='name-${newNameId}' name='names[${newNameId}]' type='text'>
+    //   </div>
+    //   `);
+
+    //   names.appendChild(s);
+    //   document.getElementById("name-"+newNameId).focus();
+    //   newNameId++;
+    // };
+
 })
