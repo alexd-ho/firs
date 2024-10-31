02ac2d7aaf4d92d469ffaed2113e1524d9399494
andymarshall-35 <167748704+andymarshall-35@users.noreply.github.com>
2024-10-31 14:58:47 +0000

    activity doer end screen

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,15 +35,24 @@ window.GOVUKPrototypeKit.documentReady(() => {
 
     function manageCard() {
         $('.show-collapsed-content').on('click', function() {
-            console.log('aaa');
+            console.log('show');
             $(this).closest('.govuk-summary-card').toggleClass('show-content');
             return false;
         })
+        $('.js-remove-card').on('click', function() {
+            console.log('remove');
+            $(this).closest('.govuk-summary-card').addClass('hidden');
+            return false;
+        })
+
+        
     };
 
     window.onload = function() {
         manageCard();
     };
+
+
 
 
 })
